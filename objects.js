95853fcb3ef6c0780a228b556e700d0080f267b6
Roscoe Sabale <rsabale43@gmail.com>
2018-01-19 07:51:45 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = { LadyGaga: 'Born This Way' }
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
